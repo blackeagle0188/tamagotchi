6d1574c38fe056fb5bacbfcdbdcae9afa403e4ad
superherodemigod <blackeagle0188@gmail.com>
2021-11-16 14:36:12 +0900

    update button

--- components/Button/index.jsx
+++ components/Button/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 
 const Button = ({text}) => {
     return (
-        <button class="bg-yellow-700 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded mx-4">
+        <button className="bg-yellow-700 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded mx-4">
             {text}
         </button>
     )
